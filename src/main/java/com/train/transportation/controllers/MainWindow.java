094dddfb4bc371d9b2c55c9d0f771f0a5a682307
SergeySmirnovJava <sergej.smirnov1998@yandex.ru>
2021-05-28 07:40:19 +0300

    Modified controllers classes

--- src/main/java/com/train/transportation/controllers/MainWindow.java
+++ src/main/java/com/train/transportation/controllers/MainWindow.java
@@ -1,4 +1,42 @@
 package com.train.transportation.controllers;
 
-public class MainWindow {
+import com.train.transportation.TransportationsStage;
+import com.train.transportation.data_base.DataBaseConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import javax.xml.transform.Source;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class MainWindow implements Initializable {
+    private Connection connection;
+    public Button statisticsManagement;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            DataBaseConnection dataBaseConnection = new DataBaseConnection();
+            connection = dataBaseConnection.getConnection();
+        } catch (SQLException e) {
+            System.out.println("Exception");
+        }
+    }
+
+    public void openTransportationStatistics(ActionEvent actionEvent) throws IOException {
+        Stage scene = new Stage();
+        TransportationsStage transportationsStage = new TransportationsStage(scene);
+        FXMLLoader transportationFxml = new FXMLLoader(getClass().getClassLoader()
+                                                                .getResource("TransportationsStatistics.fxml"));
+        Parent root = transportationFxml.load();
+        TransportationsStatistics transportationsStatistics = transportationFxml.getController();
+        transportationsStatistics.setConnection(connection);
+    }
 }
--- src/main/java/com/train/transportation/controllers/TransportationsStatistics.java
+++ src/main/java/com/train/transportation/controllers/TransportationsStatistics.java
@@ -1,4 +1,37 @@
 package com.train.transportation.controllers;
 
-public class TransportationsStatistics {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.util.ResourceBundle;
+
+public class TransportationsStatistics implements Initializable {
+    public Button stationsInfo;
+    public Button deprecatedFlight;
+    public Button shortestWay;
+    public Button canceledNumber;
+    private Connection connection;
+
+    public void setConnection(Connection connection){
+        this.connection = connection;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+    }
+    public void getStationsInfo(ActionEvent actionEvent) {
+    }
+
+    public void getCancelledFlights(ActionEvent actionEvent) {
+    }
+
+    public void getShortestWays(ActionEvent actionEvent) {
+    }
+
+    public void getNumberOfFlights(ActionEvent actionEvent) {
+    }
 }
